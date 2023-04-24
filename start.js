3ba3496864da8608a88f9e9cbda14a2880b8dc1a
nhuy29 <ny22167@gmail.com>
2023-04-24 09:23:48 +0700

    ..

---
+++ start.js
@@ -0,0 +1,58 @@
+window.addEventListener("load", function () {
+    const slider = document.querySelector(".slider");
+    const slidermain = document.querySelector(".slider-main");
+    const slideritems = document.querySelectorAll(".slider-item");
+    const nextbtn = document.querySelector(".slider-next");
+    const prevbtn = document.querySelector(".slider-prev");
+    const dotsitems = document.querySelectorAll(".slider-dot-item");
+    const slideritemWithd = slideritems[0].offsetWidth;
+    const sliderslenght = slideritems.length;
+    let postionx = 0;
+    let index = 0;
+    const dots = document.querySelector(".slider-dots");
+    nextbtn.addEventListener("click", function () {
+        handchage(1);
+    });
+    prevbtn.addEventListener("click", function () {
+        handchage(-1);
+    });
+
+    dotsitems.forEach(item => {
+        item.addEventListener("click", function (e) {
+            const sliteindex = parseInt(e.target.dataset.index)
+            index = sliteindex;
+            dotsitems.forEach(el => el.classList.remove("active"));
+            e.target.classList.add("active")
+            postionx = index * -slideritemWithd;
+            slidermain.style = `transform: translateX(${ postionx }px)`;
+        })
+    });
+
+    function handchage(direction) {
+        if (direction == 1) {
+
+            if (index >= sliderslenght - 1) {
+                index = sliderslenght - 1;
+                return;
+            }
+            postionx = postionx - slideritemWithd;
+
+            slidermain.style = `transform: translateX(${ postionx }px)`;
+            index++;
+        }
+        else if (direction == -1) {
+
+            if (index <= 0) {
+                index = 0;
+                return;
+            }
+            postionx = postionx + slideritemWithd;
+            slidermain.style = `transform: translateX(${ postionx }px)`;
+            index--;
+        }
+        dotsitems.forEach(el => el.classList.remove("active"));
+        dotsitems[index].classList.add("active");
+
+    }
+}
+);
