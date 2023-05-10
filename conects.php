8ba391344a800a16e242c65d2de16e837c4f2a3b
unknown <ble07983@gmail.com>
2023-05-10 10:49:01 +0700

    yy

--- conects.php
+++ conects.php
@@ -7,7 +7,7 @@ $dbname = "severdoan";
 $conn = new mysqli($host,$username,$password,$dbname);
 
 if($conn->connect_error){
-    die("ket noi thanh cong" .$conn->connect_error);
+    die("khong ket noi thanh cong" .$conn->connect_error);
    
 }else{
      header("location: DangNhap.php");
