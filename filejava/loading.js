8b1fa911ad1a0d99a84407501e8092afb89bd69b
unknown <ble07983@gmail.com>
2023-05-19 08:12:45 +0700

    dd

--- filejava/loading.js
+++ filejava/loading.js
@@ -7,3 +7,14 @@ function closedmenu(){
     var event = document.querySelector(".menu-classlist");
     event.style.display = "none";
 }
+
+function datve(){
+let postionx = 0;
+const slidermain = document.querySelector(".header");
+ var witditem = document.querySelector(".header").offsetWidth 
+ postionx = postionx - witditem;
+slidermain.style = `transform: translateY(${ postionx }vh)`;
+    document.querySelector(".header").style.display = "none";
+    document.querySelector(".loadi").style.display = "block";
+    document.querySelector(".main").style.display = "block";
+}
