fce0471a30771cb4320c5083d2623af5b531b509
unknown <ble07983@gmail.com>
2023-05-20 16:09:19 +0700

    h

--- xulydulieutimkiem.php
+++ xulydulieutimkiem.php
@@ -7,20 +7,39 @@ $dbname = "severdoan";
 $conn = new mysqli($host,$username,$password,$dbname);
 $sql = "SELECT * FROM tiemkiemnoiden WHERE noiden like '%$noiden%'";
 $result1 = $conn->query($sql)->fetch_assoc();
-if($result1['noiden'] == "Ha Noi" ){
-    header("location: index.html");
-}else{
-    
-    header("location: timkiem.html");
-}
-if($result1['noiden'] == "Thanh Hoa" ){
-    header("location: MienTay.html");
-}else{
-    
-    header("location: timkiem.html");
-}
-// $result = mysqli_query($conn,$sql);
-// while($row = mysqli_fetch_array($result)){
+// $arr = ['Ha Noi','Mien Tay','Mien Trung'];
+// if($result1['noiden'] == "Ha Noi" ){
 //     header("location: index.html");
-// }
+// }else{
+    
+//     header("location: timkiem.html");
+// };
+// // Mien Tay 
+// if($result1['noiden'] == "Mien Tay" ){
+//     header("location: MienTay.html");
+// }else{
+    
+//     header("location: timkiem.html");
+// };
+// // Mien Dong
+// if($result1['noiden'] == "Mien Dong" ){
+//     header("location: MienDong.html");
+// }else{
+    
+//     
+// };
+
+$result = mysqli_query($conn,$sql);
+$row = mysqli_fetch_array($result);
+ $oke =  $row['noiden'] ;
+$cach = " ";
+if($result1['noiden']){
+     header("location: $oke.html"); 
+}
+else{
+    header("location: timkiem.html");
+}
+
+   
+
 ?>
